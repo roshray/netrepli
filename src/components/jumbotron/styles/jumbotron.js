27659a4c354bd16ff60568d14748972569b969a9
roshray <roshanroy759@gmail.com>
2021-12-18 23:48:46 +0530

    footer

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -59,7 +59,7 @@ export const Image = styled.img`
 export const Container = styled.div`
     @media (max-width: 600px) {
         ${Item}:last-of-type h2 {
-            margin-bottom: 50px;
+            margin-bottom: 50px ;
         }
     }
 `
