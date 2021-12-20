0a05719742a2e033439ed44d7633021d8a683c23
roshray <roshanroy759@gmail.com>
2021-12-20 23:42:52 +0530

    faqs component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FooterContainer} from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
+import { FaqsContainer } from './containers/faqs';
 
 export default function App() {
   return(
      <>
           <JumbotronContainer />
-          <FooterContainer />
+          <FaqsContainer />
+          <FooterContainer />   
      </>
   );
 }
