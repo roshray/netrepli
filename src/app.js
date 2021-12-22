db2878cfa408652ba44006e098075040d4b97aa2
roshray <roshanroy759@gmail.com>
2021-12-22 23:53:47 +0530

    create React Router Routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FaqsContainer } from './containers/faqs';
+import { Home, Browse, Signin, Signup } from './pages';
+import * as ROUTES from './constants/routes';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
 
 export default function App() {
   return (
-     <>
-          <JumbotronContainer />
-          <FaqsContainer />
-          <FooterContainer />   
-     </>
+    <Router>
+      <Route exact path="/browse">
+        <Browse />       
+      </Route>
+      <Route exact path="/signin">
+        <Signin />       
+      </Route>
+      <Route exact path="/signup">
+        <Signup />       
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
+
   );
 }
--- src/constants/route.js
+++
@@ -1,4 +0,0 @@
-export const HOME = './';
-export const Browse = './browse';
-export const SIGN_UP = './signup';
-export const SIGN_IN = 'signin';
